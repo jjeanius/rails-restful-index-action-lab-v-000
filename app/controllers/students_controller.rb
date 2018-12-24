5120af704c76519cc68e5fdd13ec933005f534f9
Judy McGoldrick <jjeanius@verizon.net>
2018-12-24 04:50:34 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,11 @@
 class StudentsController < ApplicationController
 
  def index
+<<<<<<< HEAD
    @students = Student.all
 
  end
+=======
+   render :plain => @students = Student.all
+>>>>>>> dfe0aaedac349d7f0d6b9113e1b203c6691842db
 end
